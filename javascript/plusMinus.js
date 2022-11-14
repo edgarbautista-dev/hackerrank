8020e41f9f72a8e93385dee0ad0bd0bc4232dfe2
Vellmond <edgar.bautista@edu.uag.mx>
2022-11-14 12:05:43 -0600

    week preparaton kit

---
+++ javascript/plusMinus.js
@@ -0,0 +1,83 @@
+/* given an array  of integers, calculate the ratios of its elements that are positive, negative, and zero.
+Print the decimal value of each fraction on a new line with  places after the decimal.
+Note: This challenge introduces precision problems. The test cases are scaled to six decimal places,
+though answers with absolute error of up to  are acceptable.
+For example, given the array  there are  elements, two positive, two negative and one zero.
+Their ratios would be ,  and . It should be printed as
+0.400000
+0.400000
+0.200000
+Function Description
+Complete the plusMinus function in the editor below.
+plusMinus has the following parameter(s):
+int arr[n]: an array of integers
+Print
+Print the ratios of positive, negative and zero values in the array.
+Each value should be printed on a separate line with  digits after the decimal.
+The function should not return a value.
+Input Format
+The first line contains an integer, , the size of the array.
+The second line contains  space-separated integers that describe .
+Constraints
+ */
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    // Write your code here
+    let positive = 0;
+    let negative = 0;
+    let zero = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            positive++;
+        } else if (arr[i] < 0) {
+            negative++;
+        } else {
+            zero++;
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        if (i == 0) {
+            console.log(positive / arr.length);
+        } else if (i == 1) {
+            console.log(negative / arr.length);
+        } else {
+            console.log(zero / arr.length);
+        }
+    }
+
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
