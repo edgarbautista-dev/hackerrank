cf659e9da7cbfefbb65f5aaddfca8fff394b56f3
Vellmond <edgar.bautista@edu.uag.mx>
2022-11-14 16:45:54 -0600

    add miniMax

---
+++ javascript/miniMaxSum.js
@@ -0,0 +1,56 @@
+/*
+Given five positive integers, find the minimum and maximum values that can be calculated
+by summing exactly four of the five integers.
+ Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+ */
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    // Write your code here
+    let min = arr[0];
+    let max = arr[0];
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        sum += arr[i];
+    }
+    console.log(sum - max, sum - min);
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
